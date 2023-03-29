2005f4ca8185d6abb2bfffda79874aeed872dca8
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-03-29 16:09:35 +0300

    #3 login

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name='home'),
+    path("logout/", views.logout_user, name='logout'),
 
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def home(request):
-    return render(request, 'home.html')
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You've been logged in")
+            return redirect("home")
+        else:
+            messages.warning(request, "A mistake occured, Try one more time")
+            return redirect("home")
+    else:
+        return render(request, "home.html")
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You logged out")
+    return redirect("home")
